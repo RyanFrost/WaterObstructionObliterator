5b33dd2a938384759b68a544e7d2a940193314d2
RyanDesktopLinux <ryanwindsorfrost@gmail.com>
2015-04-01 02:38:02 -0700

    Mac-side socket comm

--- MacSide/main.cpp
+++ MacSide/main.cpp
@@ -1,6 +1,9 @@
-#include <array>
+
 #include "KeyboardInput.h"
 #include "SocketManager.h"
+#include <array>
+#include <vector>
+#include <iostream>
 
 
 
@@ -10,9 +13,16 @@ int main()
 	KeyboardInput keyboard;
 	SocketManager piComm(27015);
 
-
+	std::vector<char> data = piComm.recvData();
+	piComm.loadIntToBuf(1);
+	piComm.sendBuf();
+	for ( std::vector<char>::iterator it = data.begin(); it != data.end(); it++)
+	{
+		std::cout << *it;
+	}
+	std::cout << std::endl;
 	std::array<bool,5> keyStates;
-	while(true)
+	while(false)
 	{
 		keyStates = keyboard.getKeyPadStates();
 		if (keyStates[4] == true)
